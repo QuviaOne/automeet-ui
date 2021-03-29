a01362e8c14c47302776e429ad43f4bb5179f4be
Matej Holubec <matej.holubec@gmail.com>
2021-03-29 15:23:37 +0200

    feat: basic main screen and initialize vue script

---
+++ components/Heading.vue
@@ -0,0 +1,15 @@
+<template>
+  <CBox :w="['95%', '80%', '50%', '30%']" align="center" mx="auto">
+    <CHeading text-align="center" mb="4"> ⚡️ AutoMeet. </CHeading>
+    <CFlex justify="center" direction="column" align="center">
+      <CText>AutoMeet allows you to join a meeting automatically, without having to deal with any bullshit Šperl has prepared for ya</CText>
+    </CFlex>
+  </CBox>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,19 +7,36 @@
       flex-dir="column"
       justify-content="center"
     >
-      <CHeading text-align="center" mb="4"> ⚡️ AutoMeet. </CHeading>
-      <CFlex justify="center" direction="column" align="center">
-        <CBox mb="3">
-          <CButton left-icon="info" variant-color="blue"> Let's go </CButton>
-        </CBox>
-      </CFlex>
+      <Heading />
+      <CBox :w="['95%', '80%', '50%', '30%']" mt="10" mx="auto">
+        <CInput placeholder="Meet Address" v-model="inputs.address" />
+        <CButton @click="runMeet">Go there</CButton>
+      </CBox>
     </CBox>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Heading from '~/components/Heading.vue'
 export default Vue.extend({
   name: 'App',
+  components: { Heading },
+  data() {
+    return {
+      inputs: {
+        address: '',
+      },
+    }
+  },
+  methods: {
+    runMeet() {
+      //@ts-ignore
+      this.$toast({
+        title: "This does not work yet...",
+        description: this.inputs.address
+      })
+    }
+  },
 })
 </script>
