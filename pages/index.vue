5636a26baff857d18105f4cc15aaf1ab02b5bbb9
Matej Holubec <matej.holubec@gmail.com>
2021-03-30 11:14:07 +0200

    feat: add meetal inputs

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,20 @@
     >
       <Heading />
       <CBox :w="['95%', '80%', '50%', '30%']" mt="10" mx="auto">
-        <CInput placeholder="Meet Address" v-model="inputs.address" />
+        <CInput placeholder="Login Address" v-model="inputs.email" />
+        <CInput
+          placeholder="Password"
+          type="password"
+          v-model="inputs.password"
+        />
+
+        <CInput placeholder="Meet Code" v-model="inputs.address" />
+        <CInput
+          placeholder="Custom Meet Load Wait Time"
+          v-model="inputs.customWait"
+        />
+        <CInput placeholder="Meet Duration" v-model="inputs.meetDuration" />
+        <br />
         <CButton @click="runMeet">Go there</CButton>
       </CBox>
     </CBox>
@@ -18,7 +31,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import fs from 'fs'
+import { execFile } from 'child_process'
 import Heading from '~/components/Heading.vue'
+
 export default Vue.extend({
   name: 'App',
   components: { Heading },
@@ -26,17 +42,31 @@ export default Vue.extend({
     return {
       inputs: {
         address: '',
+        email: '',
+        password: '',
+        customWait: undefined,
+        meetDuration: undefined,
       },
     }
   },
   methods: {
     runMeet() {
+      fs.writeFileSync(
+        '~/creds.json',
+        `{\n    "email": "${this.inputs.email}",\n    "password": "${this.inputs.password}"\n}`
+      )
+      execFile('../index.ts', [
+        `https://meet.google.com/${this.inputs.address}`,
+        String(this.inputs.meetDuration),
+        String(this.inputs.customWait),
+      ])
+
       //@ts-ignore
       this.$toast({
-        title: "This does not work yet...",
-        description: this.inputs.address
+        title: 'Launching',
+        description: `meet.google.com/${this.inputs.address}`,
       })
-    }
+    },
   },
 })
 </script>
